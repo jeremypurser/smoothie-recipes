82ee5bec2171a3f4b1ddd1025ec91ce524c1e0d1
Jeremy Purser <jeremypurser@gmail.com>
2021-05-23 14:23:40 -0400

    Add local storage helpers

---
+++ src/app/localStorage.ts
@@ -0,0 +1,22 @@
+import { RootState } from './store';
+
+export function loadState() {
+  try {
+    const serialized = window.localStorage.getItemg('state');
+    if (serialized === null) {
+      return undefined;
+    }
+    return JSON.parse(serialized);
+  } catch (e) {
+    return undefined;
+  }
+}
+
+export function saveState(state: RootState) {
+  try {
+    const serialized = JSON.stringify(state);
+    window.localStorage.setItem('state', serialized);
+  } catch (e) {
+    console.error(e);
+  }
+}
