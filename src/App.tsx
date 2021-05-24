dfc6fe3f2c8e61299328a6d7de05a3b55c07ccd4
Jeremy Purser <jeremypurser@gmail.com>
2021-05-24 18:55:27 -0400

    Add router and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import { SmoothieForm } from './components/SmoothieForm';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { Header } from './components/Header';
+import { SmoothieForm } from './components/SmoothieForm';
+import { Smoothies } from './components/Smoothies';
 
 function App() {
-  return <SmoothieForm />;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/recipe">
+          <SmoothieForm />
+        </Route>
+        <Route path="/recipe/:id">
+          <SmoothieForm />
+        </Route>
+        <Route path="/recipes">
+          <Smoothies />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
