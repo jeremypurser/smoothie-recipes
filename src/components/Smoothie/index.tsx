f0193754968f0bf36b1ebfb987ec0787877ff256
Jeremy Purser <jeremypurser@gmail.com>
2021-05-24 21:29:31 -0400

    Style smoothie card

--- src/components/Smoothie/index.tsx
+++ src/components/Smoothie/index.tsx
@@ -1,6 +1,7 @@
 import { useHistory } from 'react-router-dom';
 import { useAppDispatch } from '../../app/hooks';
 import { deleteRecipe, Recipe } from '../../data/recipesSlice';
+import style from './style.module.css';
 import { pluralize } from './utility';
 
 export function Smoothie({ recipe }: { recipe: Recipe }) {
@@ -8,19 +9,19 @@ export function Smoothie({ recipe }: { recipe: Recipe }) {
   const history = useHistory();
 
   return (
-    <>
+    <div className={style.card}>
       <h2>{recipe.name}</h2>
       {recipe.ingredients.map((ingredient) => (
         <div key={ingredient.name}>
-          <span>{ingredient.name}</span>
-          <span>{ingredient.amount.value}</span>
+          <span>{ingredient.amount.value} </span>
           <span>
-            {pluralize(ingredient.amount.value, ingredient.amount.unit)}
+            {pluralize(ingredient.amount.value, ingredient.amount.unit)}{' '}
           </span>
+          <span>{ingredient.name}</span>
         </div>
       ))}
       <button onClick={() => history.push(`/recipe/${recipe.id}`)}>Edit</button>
       <button onClick={() => dispatch(deleteRecipe(recipe.id))}>Delete</button>
-    </>
+    </div>
   );
 }
